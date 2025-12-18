973c435259659273436df0ec80b145842b1cfdec
PavolUlicny <pavolko.u@gmail.com>
2025-12-18 22:26:44 +0100

    tests: remove duplicate embedding not-found test (already covered)

--- tests/test_embedding_client_extra.py
+++ tests/test_embedding_client_extra.py
@@ -1,27 +1,6 @@
 from __future__ import annotations
 
 
-def test_build_client_not_found_calls_handle_missing_model(monkeypatch):
-    from src import embedding_client as EC
-
-    class FailCtor:
-        def __init__(self, *a, **k):
-            raise Exception("Model not found: foo")
-
-    monkeypatch.setattr(EC, "OllamaEmbeddings", FailCtor)
-
-    called = {}
-
-    def fake_handle_missing_model(*args, **kwargs):
-        called["ok"] = True
-
-    monkeypatch.setattr(EC._model_utils_mod, "handle_missing_model", fake_handle_missing_model)
-
-    c = EC.EmbeddingClient("foo")
-    assert c.embed("text") is None
-    assert called.get("ok") is True
-
-
 def test_embed_returns_none_and_logs_on_exception(monkeypatch, caplog):
     from src import embedding_client as EC
 
